636b4ea826b8a996b77fb0b18ef5eb78599c3059
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-04 21:45:35 -0700

    Added gems: factory_girl, rspec, rspec-rails.

--- railsapp/config/environment.rb
+++ railsapp/config/environment.rb
@@ -19,6 +19,11 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  if RAILS_ENV == "test"
+    config.gem "factory_girl"
+    config.gem "rspec", :lib => false
+    config.gem "rspec-rails", :lib => false
+  end
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named

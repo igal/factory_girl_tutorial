488199846946d11db5d4f58678b49223455a97ac
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-04 22:01:27 -0700

    Added kitten with custom factory spec.

--- railsapp/spec/models/kitten_spec.rb
+++ railsapp/spec/models/kitten_spec.rb
@@ -9,4 +9,18 @@ describe Kitten do
       kitten.legs.should be_a_kind_of(Fixnum)
     end
   end
+
+  describe "with custom factory" do
+    def create_kitten(name=nil, legs=nil)
+      @name ||= 0
+      name ||= "kitten-#{@name += 1}"
+      legs ||= 4
+      return Kitten.create(:name => name, :legs => legs)
+    end
+
+    it "should have a number of legs" do
+      kitten = create_kitten("Shiva The Destroyer", 6)
+      kitten.legs.should be_a_kind_of(Fixnum)
+    end
+  end
 end
